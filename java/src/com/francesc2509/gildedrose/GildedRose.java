10f12f4a30349c2b6cb2d8dd5f6a833437df4be0
Francesc Manarel Bueno <francesc2509@outlook.com>
2021-01-29 23:12:23 +0000

    Add Java project

---
+++ java/src/com/francesc2509/gildedrose/GildedRose.java
@@ -0,0 +1,13 @@
+package com.francesc2509.gildedrose;
+
+public class GildedRose {
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+
+    }
+}
